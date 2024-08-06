a122c75ca0086b1b05743a05499b462fb1c86ade
ZooSOS <wbin90620@gmail.com>
2024-08-06 13:09:19 -0700

    updated with some description

---
+++ src/images/index.ts
@@ -0,0 +1,15 @@
+import bear from "./bear.png";
+import coin from "./coin.png";
+import highVoltage from "./high-voltage.png";
+import zoocoin from "./zoocoin.png";
+import rocket from "./rocket.png";
+import trophy from "./trophy.png";
+
+export {
+    bear,
+    coin,
+    highVoltage,
+    zoocoin,
+    rocket,
+    trophy
+}
